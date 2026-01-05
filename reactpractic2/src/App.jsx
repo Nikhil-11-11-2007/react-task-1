5b6a5c1328c04dd6f4ad628c43d9b169ee795e80
Nikhil-11-11-2007 <ng364357@gmail.com>
2026-01-05 12:00:20 +0530

    browserRouter

--- reactpractic2/src/App.jsx
+++ reactpractic2/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom"
+import { createBrowserRouter, Route, RouterProvider, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
 import About from "./pages/About"
@@ -7,12 +7,49 @@ import Java from "./pages/Java"
 import Python from "./pages/Python"
 import AllCourses from "./pages/AllCourses"
 import Footer from "./components/Footer"
+import Layout from "./pages/Layout"
 
 const App = () => {
+
+  const allrouter = createBrowserRouter([
+    {
+      path:'/',
+      element:<Layout />,
+      children:[
+    {
+      index:true,
+      element:<Home/>
+    },
+    {
+      path:'/about',
+      element:<About/>
+    },
+    {
+      path:'/courses',
+      element:<Courses/>,
+      children:[
+        {
+          path:'/courses',
+          element:<AllCourses/>
+        },
+        {
+          path:'/courses/java',
+          element:<Java/>
+        },
+        {
+          path:'/courses/python',
+          element:<Python/>
+        }
+      ]
+    },
+    
+  ]
+    }
+  ])
   return (
     <div className='h-screen bg-black text-white'>
-      <Navbar />
-      <Routes>
+      {/* <Navbar /> */}
+      {/* <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/courses" element={<Courses />}>
@@ -20,10 +57,37 @@ const App = () => {
           <Route path="/courses/java" element={<Java />} />
           <Route path="/courses/python" element={<Python />} />
         </Route>
-      </Routes>
-      <Footer />
+      </Routes> */}
+      <RouterProvider router={allrouter} />
+      {/* <Footer /> */}
     </div>
   )
 }
 
 export default App
+
+// [
+//     {
+//       path:'/',
+//       element:<Home/>
+//     },
+//     {
+//       path:'/about',
+//       element:<About/>
+//     },
+//     {
+//       path:'/courses',
+//       element:<Courses/>,
+//       children:[
+//         {
+//           path:'/courses/java',
+//           element:<Java/>
+//         },
+//         {
+//           path:'/courses/python',
+//           element:<Python/>
+//         }
+//       ]
+//     },
+    
+//   ]
